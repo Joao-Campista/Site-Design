e1f8585b250f61f88d912ab4eec80f5840e31a33
Campista <devjpcampista@gmail.com>
2025-02-18 18:00:22 -0300

    seção portfolio pronta

--- script.js
+++ script.js
@@ -27,12 +27,43 @@ function type() {
 
         setTimeout(() => {
             type();
-        }, 500);
+        }, 300);
     } else {
         setTimeout(() => {
             type();
-        }, 100);
+        }, 30);
     }
 }
+
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
+
+
+const botao = document.querySelector('.next');
+
+
+function simularClique() {
+    if (botao) {
+        botao.click();
+    } else {
+    }
+}
+
+
+const intervalo = 10000;
+
+
+setInterval(simularClique, intervalo);
+
 
 type();
